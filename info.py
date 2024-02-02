60bc5ab095cbc6983ac1ab440e351ef4ca71eaa5
hannahbaker01 <134852510+hannahbaker01@users.noreply.github.com>
2024-02-02 17:28:21 +0530

    ok

--- info.py
+++ info.py
@@ -65,7 +65,7 @@ UPDATES_LINK = environ.get('UPDATES_LINK', 'https://t.me/wd_link')
 
 # Bot settings
 AUTO_FILTER = is_enabled((environ.get('AUTO_FILTER', "True")), True)
-IMDB = is_enabled((environ.get('IMDB', "True")), True)
+IMDB = is_enabled((environ.get('IMDB', "False")), True)
 SPELL_CHECK = is_enabled(environ.get("SPELL_CHECK", "False"), False)
 SHORTLINK = is_enabled((environ.get('SHORTLINK', "False")), False)
 DELETE_TIME = int(environ.get('DELETE_TIME', 3600)) # Add time in seconds
@@ -73,7 +73,7 @@ AUTO_DELETE = is_enabled((environ.get('AUTO_DELETE', "True")), True)
 WELCOME = is_enabled((environ.get('WELCOME', "True")), True)
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
-LINK_MODE = is_enabled(environ.get("LINK_MODE", "True"), True)
+LINK_MODE = is_enabled(environ.get("LINK_MODE", "False"), True)
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 
 # Other
